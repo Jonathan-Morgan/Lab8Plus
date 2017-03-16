91da6bbf871f64ccd2c70a538e44b2e23b253623
Jon <morganusn@gmail.com>
2017-03-16 05:38:28 +0000

    created products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   
+# root 'application#hello'
+
+ root 'products#index'
   
-  root 'application#hello'
 end
